fa8fcb010f63ccdf46775855199f767b911c4138
bhavikachopra06 <choprabhavika83@gmail.com>
2026-02-11 09:12:52 +0530

    Python Basics 🚀

---
+++ control_flow.py
@@ -0,0 +1,51 @@
+marks = int(input("Enter marks: "))
+if marks > 90:
+    print("Excellent")
+elif 90> marks > 80:
+    print("Awesome")
+else:
+    print("Good")
+
+print(0 and 5)  # AND -> returns first falsy value (0)
+print(1 and 5)  # AND -> returns last value if all are truthy (5)
+print(1 or 5)   # OR -> returns first truthy value (1)
+print(0 or 5)   # OR -> returns first truthy value (5)
+print(not 0)    # NOT -> flips boolean (True)
+print(not 1)    # NOT -> flips boolean (False)
+print(not 5)    # NOT -> flips boolean (False)
+
+#for loop with list
+l = [1,2,3,4]
+for value in l:
+    print(value)
+
+#for loop with tuple
+t = (1,2,3,4)
+for val in t:
+    print(val)
+
+#for loop with set
+s = {1,2,3,4}
+for v in s:
+    print(v)
+
+#for loop with dictionary
+d = {"name" : "Bhavika", "age" : 20}
+for key in d.keys():
+    print(key)
+for value in d.values():
+    print(value)
+for key, value in d.items():
+    print(key, value)
+
+#for loop with range
+l = [1,2,3,4]
+for i in range(0,3):
+    print(i)
+
+#while loop
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+
---
+++ functions_and_functional_thinking.py
@@ -0,0 +1,39 @@
+def greet():     #function definition
+    print("Hello")
+greet()    #function call
+
+def greet(name):  #Parameter
+    print(f"Hello {name}")
+greet("Bhavika") #Argument
+
+def func():
+    a = 10
+    b = 15
+    add = a+b
+    return add
+print(func()) #output -> 25
+
+#Function without return keyword
+def func():
+    a = 10
+    b = 5
+    add = a + b
+    print(add)
+print(func()) # output will be: # 15 None
+
+# *args allows function to accept any number of positional arguments as a tuple
+def add(*args):
+    total = 0
+    for x in args:
+        total += x
+    return total
+
+
+print(add(1, 2))
+print(add(1, 2, 3))
+print(add(1, 2, 3, 4)) # it can take any number of arguments
+
+# **kwargs allow function to accept any number of keyword arguments as a dictionary
+def info(**kwargs):
+    print(kwargs)
+info(name="bhavika", age=20)
