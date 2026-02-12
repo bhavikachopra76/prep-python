86ce596c7fd3e4549416eada5e286651fa068ed4
bhavikachopra06 <bhavikachopra67@gmail.com>
2026-02-12 13:34:46 +0530

    OOPS Concepts 🚀

--- oops/oops_basics.py
+++ oops/oops_basics.py
@@ -13,3 +13,51 @@ def func(sd):
 
 s1 = Student("Bhavika", 21, 98.5)
 func(s1)
+
+
+#Create student class that takes name & marks of 3 subjects as arguments in constructor.
+# Then create a method to print the average.
+
+class StudentInfo:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+    def average_marks(self):
+        add = sum(self.marks)
+        avg = add/len(self.marks)
+        return avg
+
+stu1 = StudentInfo("Bhavika", [80,90,95])
+print(stu1.name)
+print(stu1.marks)
+print(stu1.average_marks())
+
+#Create Account class with 2 attributes - balance & account no.
+#Create methods for debit, credit & printing the balance.
+class Account:
+    @staticmethod
+    def greeting(): #Static Method
+        print("Your account has been stored to database")
+
+    def __init__(self, balance, account_no):
+        self.balance = balance
+        self.account_no = account_no
+
+    def debit_money(self, amount):
+        self.balance = self.balance - amount
+        print(f"₹{amount} Debited from account no. {self.account_no} ")
+
+    def credit_money(self, amount):
+        self.balance = self.balance + amount
+        print(f"₹{amount} Credited to account no. {self.account_no} ")
+
+    def get_balance(self):
+        print(f"Balance in your Account: ₹{self.balance}")
+
+Account1 = Account(1000, 855)
+Account1.greeting()
+Account1.get_balance()
+Account1.debit_money(100)
+Account1.get_balance()
+Account1.credit_money(200)
+Account1.get_balance()
